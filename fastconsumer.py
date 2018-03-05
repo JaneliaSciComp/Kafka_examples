75ea4d3a7dd0d898c3b719e3d303a50a57751420
Rob Svirskas <svirskasr@janelia.hhmi.org>
2018-03-05 15:00:08 -0500

    Added additional config options to fastconsumer.py

--- fastconsumer.py
+++ fastconsumer.py
@@ -1,17 +1,38 @@
+import argparse
+import sys
 from confluent_kafka import Consumer, KafkaError
 
-c = Consumer({'bootstrap.servers': 'kafka', 'group.id': 'mygroup',
-              'default.topic.config': {'auto.offset.reset': 'smallest'}})
-c.subscribe(['test'])
-running = True
-while running:
-    msg = c.poll()
-    if not msg.error():
-        print ("%s:%s:%d: key=%s value=%s" % (msg.topic(), msg.partition(),
-                                              msg.offset(),
-                                              str(msg.key()),
-                                              msg.value().decode('utf-8')))
-    elif msg.error().code() != KafkaError._PARTITION_EOF:
-        print(msg.error())
-        running = False
-c.close()
+
+def read_messages(server, topic, group, offset):
+    if not server:
+        server = 'kafka,kafka2,kafka3'
+    if not group:
+        group = None
+    cons = Consumer({'bootstrap.servers': server, 'group.id': group,
+                     'default.topic.config': {'auto.offset.reset': offset}})
+    cons.subscribe([topic])
+    running = True
+    while running:
+        msg = cons.poll()
+        if not msg.error():
+            print ("%s:%s:%d: key=%s value=%s" % (msg.topic(), msg.partition(),
+                                                  msg.offset(),
+                                                  str(msg.key()),
+                                                  msg.value().decode('utf-8')))
+        elif msg.error().code() != KafkaError._PARTITION_EOF:
+            print(msg.error())
+            running = False
+    cons.close()
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(
+        description='Kafka consumer (confluent_kafka)')
+    PARSER.add_argument('--server', dest='server', default='', help='Server')
+    PARSER.add_argument('--topic', dest='topic', default='test', help='Topic')
+    PARSER.add_argument('--group', dest='group', default='', help='Group')
+    PARSER.add_argument('--offset', dest='offset', default='earliest',
+                        help='offset (earliest or latest)')
+    ARGS = PARSER.parse_args()
+    read_messages(ARGS.server, ARGS.topic, ARGS.group, ARGS.offset)
+    sys.exit(0)
