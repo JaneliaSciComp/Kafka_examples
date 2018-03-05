9fce3955189e017fc359ff0d5f264785a381a997
Rob Svirskas <svirskasr@janelia.hhmi.org>
2018-03-05 13:51:11 -0500

    Added additional config options to consumer.py

--- consumer.py
+++ consumer.py
@@ -1,12 +1,19 @@
 import argparse
-from kafka import KafkaConsumer
 import sys
+from kafka import KafkaConsumer
 
 
-def readMessages(server, topic):
+def read_messages(server, topic, group, offset):
+    if server:
+        server_list = [server + ':9092']
+    else:
+        server_list = ['kafka:9092', 'kafka2:9092', 'kafka3:9092']
+    if not group:
+        group = None
     consumer = KafkaConsumer(topic,
-                             bootstrap_servers=[server + ':9092'],
-                             auto_offset_reset='smallest')
+                             bootstrap_servers=server_list,
+                             group_id=group,
+                             auto_offset_reset=offset)
     for message in consumer:
         print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                               message.offset, message.key,
@@ -14,11 +21,13 @@ def readMessages(server, topic):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
+    PARSER = argparse.ArgumentParser(
         description='Kafka consumer')
-    parser.add_argument('-server', dest='server', default='kafka',
-                        help='Server')
-    parser.add_argument('-topic', dest='topic', default='test', help='Topic')
-    args = parser.parse_args()
-    readMessages(args.server, args.topic)
+    PARSER.add_argument('--server', dest='server', default='', help='Server')
+    PARSER.add_argument('--topic', dest='topic', default='test', help='Topic')
+    PARSER.add_argument('--group', dest='group', default='', help='Group')
+    PARSER.add_argument('--offset', dest='offset', default='earliest',
+                        help='offset (earliest or latest)')
+    ARGS = PARSER.parse_args()
+    read_messages(ARGS.server, ARGS.topic, ARGS.group, ARGS.offset)
     sys.exit(0)
