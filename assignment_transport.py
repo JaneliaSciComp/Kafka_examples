6c9d05343a8fb5a9c74537702e01ec639c5aaec1
Rob Svirskas <svirskasr@janelia.hhmi.org>
2018-10-16 15:15:06 -0400

    First effort to find working hours for assignment_transport.py

--- assignment_transport.py
+++ assignment_transport.py
@@ -4,9 +4,18 @@ import sys
 import colorlog
 import requests
 import MySQLdb
+from business_duration import businessDuration
+import pandas as pd
+import datetime
+import holidays as pyholidays
+import time
 
 # Database
-READ = {'completed': "SELECT id,user,annotation,type,UNIX_TIMESTAMP(start_date),"
+READ = {'all': "SELECT id,user,annotation,type,UNIX_TIMESTAMP(start_date),"
+               + "UNIX_TIMESTAMP(complete_date),"
+               +  "UNIX_TIMESTAMP(complete_date)-UNIX_TIMESTAMP(start_date) "
+               + "FROM assignment_vw WHERE start_date > 0 ORDER BY start_date",
+        'completed': "SELECT id,user,annotation,type,UNIX_TIMESTAMP(start_date),"
                      + "UNIX_TIMESTAMP(complete_date),"
                      + "UNIX_TIMESTAMP(complete_date)-UNIX_TIMESTAMP(start_date) "
                      + "FROM assignment_vw WHERE is_complete=1 AND UNIX_TIMESTAMP(complete_date)>0"
@@ -75,14 +84,23 @@ def initialize_program():
 
 
 def process_mad(filename):
+    open_time = datetime.time(6, 0, 0)
+    close_time = datetime.time(18, 0, 0)
+    holidaylist = pyholidays.US()
     try:
-        CURSOR['mad'].execute(READ['completed'], )
+        CURSOR['mad'].execute(READ['all'], )
     except MySQLdb.Error as err:
         sql_error(err)
     handle = open(filename, 'w') if filename else sys.stdout
     rows = CURSOR['mad'].fetchall()
     workdict = dict()
     for row in rows:
+        if row[5] and (row[5] < row[4] or not row[4]):
+            continue
+        if (not row[5]) and ARG.COMPLETED:
+            continue
+        if (row[5]) and not ARG.COMPLETED:
+            continue
         org = ''
         if row[1] in workdict:
             org = workdict[row[1]]
@@ -91,17 +109,34 @@ def process_mad(filename):
             if 'config' in workday and 'organization' in workday['config']:
                 org = workday['config']['organization']
             workdict[row[1]] = org
-        rec = {'id': row[0], 'user': row[1], 'annotation': row[2], 'type': row[3],
-               'start_time': row[4], 'end_time': row[5], 'duration': row[6], 'organization': org}
+        completed = 1 if row[5] else 0
+        duration = row[6] if (row[6] > 0) else 0
+        if row[5]:
+            startstring = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(row[4]))
+            endstring = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(row[5]))
+            startdate = pd.to_datetime(startstring)
+            enddate = pd.to_datetime(endstring)
+            working_duration = businessDuration(startdate,enddate,open_time,close_time,
+                                                holidaylist=holidaylist,unit='hour') * 3600.0
+            logger.debug("Working duration %s-%s is %f hr" % (startdate, enddate, working_duration))
+        else:
+            working_duration = 0
+        rec = {'id': row[0], 'user': row[1], 'organization': org,
+               'annotation': row[2],'type': row[3],
+               'start_time': row[4], 'end_time': row[5], 'duration': duration,
+               'working_duration': working_duration
+               }
         handle.write(json.dumps(rec) + "\n")
     if handle is not sys.stdout:
         handle.close()
 
 
 if __name__ == '__main__':
-    PARSER = argparse.ArgumentParser(description="Dump completed asssignmens from MAD to a JSON file")
+    PARSER = argparse.ArgumentParser(description="Dump asssignments from MAD to a JSON file")
     PARSER.add_argument('--file', dest='FILE', action='store',
-                        default='', help='File containing lines or cross barcodes')
+                        default='', help='File containing assignments')
+    PARSER.add_argument('--completed', dest='COMPLETED', action='store_true',
+                        default=False, help='Completed assignments only')
     PARSER.add_argument('--verbose', dest='VERBOSE', action='store_true',
                         default=False, help='Flag, Chatty')
     PARSER.add_argument('--debug', dest='DEBUG', action='store_true',
